280ded1e936a874b11f1c70b14eaccd461a7dae8
vodono <vdn2004@gmail.com>
2019-04-30 01:22:47 +0300

    Add gunicorn

--- src/main.py
+++ src/main.py
@@ -2,10 +2,21 @@ import requests
 
 token = "DwfRsoxSUaDxV-jFatXz9EWMZ_3i_UsH-FF4yWeW"
 
-orders = requests.get('https://api-sandbox.coingate.com/v2/orders',
-                      headers={'Authorization': f'Token {token}'})
+def app(environ, start_response):
+    data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
 
-print(orders)
+def main():
+    orders = requests.get('https://api-sandbox.coingate.com/v2/orders',
+                      headers={'Authorization': f'Token {token}'})
+    print(orders)
 
 # import ipdb
 # ipdb.set_trace()
+
+if __name__ == "__main__":
+    main()
